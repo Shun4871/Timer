f2b54780a2dffa419ac310c2c3b254428be92cf4
ShunnosukeTsuge <shuntsugework@gmail.com>
2024-09-09 07:46:28 +0900

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by 柘植俊之介 on 2024/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var timer: Timer!
+    var countdown: Int = 0
+    
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func startTimer(time: Int){
+        countdown = time
+        timer = Timer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    @IBAction func select30seconds(){
+        startTimer(time: 30)
+    }
+    @IBAction func select60seconds(){
+        startTimer(time: 60)
+    }
+    @IBAction func select5min(){
+        startTimer(time: 300)
+    }
+    @IBAction func select10min(){
+        startTimer(time: 600)
+    }
+    @objc func onTimerCalled(){
+        let remainingMinutes: Int = countdown / 60
+        let remainingSeconds: Int = countdown % 60
+        label.text = String (format: "%02d:%02d", remainingMinutes,
+                             remainingSeconds)
+        countdown -= 1
+        if countdown < 0 {
+            let stopAlert = UIAlertController(title: "タイマーが終了しました",
+                message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            stopAlert.addAction (okAction)
+            present(stopAlert, animated: true)
+            timer.invalidate()
+        }
+    }
+}
